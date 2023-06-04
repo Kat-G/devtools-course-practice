86b3f5144a8506c3d153e843da68e8a72dc22741
Kat-G <katylaky@outlook.com>
2023-06-04 18:02:45 +0300

    Update Function_Eyler.cpp

--- modules/Function_Eyler/src/Function_Eyler.cpp
+++ modules/Function_Eyler/src/Function_Eyler.cpp
@@ -8,19 +8,22 @@ int Function_Eyler::Function_Euler(int n) {
     if (n <= 0) {
         throw std::invalid_argument(
         "Invalid value of argument. Must be positive");
-    }int count = 0;
-    std::vector<bool> isPrime(n, true);
-
-    for (int i = 2; i <= sqrt(n); i++) {
-        if (isPrime[i]) {
-            for (int j = i * i; j <= n; j += i) {
-                isPrime[j] = false;
-            }
-        }
+    }
+    if (n == 1 || n == 2) {
+        return n;
     }
 
-    for (int i = 2; i <= n; i++) {
-        if (isPrime[i]) {
+    int count = 1;
+
+    for (int i = 3; i <= n / 2; i++) {
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
             count++;
         }
     }
